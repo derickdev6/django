2d1bb0d56b92193cb921e5ef4525a9d2cf1e48e0
DerickPCLinux <derickdev6@gmail.com>
2021-08-14 00:44:04 -0500

    Starting ORM created Model, makemigrations, migrate

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,18 @@
+# Django
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    """User model."""
+
+    email = models.EmailField(unique=True)
+
+    password = models.CharField(max_length=100)
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    is_premium = models.BooleanField()
+
+    bio = models.TextField(blank=True)
+    birth_date = models.DateField(blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
